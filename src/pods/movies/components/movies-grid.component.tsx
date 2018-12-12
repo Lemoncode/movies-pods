d5f2c8622b71f8cf3e8a12979f26711969c9fd1b
jimenc23 <cristian.jimenez@roche.com>
2018-12-12 17:43:23 +0100

    Componentize and moved mock data

--- src/pods/movies/components/movies-grid.component.tsx
+++ src/pods/movies/components/movies-grid.component.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { MovieEntity } from '../view-model/movie';
-import { MoviesGridContainer } from '../movies-grid.container';
+import { MovieGridHead } from './grid/movies-grid-head.component';
+import { MovieGridBody } from './grid/movies-grid-body.component';
+import { MovieEntity } from './view-model';
 
 interface Props {
   movieList: MovieEntity[];
@@ -17,28 +13,8 @@ const MoviesGridComponentInner = (props: Props) => {
   return (
     <Paper >
       <Table >
-        <TableHead>
-          <TableRow>
-            <TableCell>Movie</TableCell>
-            <TableCell align="right">Genre</TableCell>
-            <TableCell align="right">Age rating</TableCell>
-            <TableCell align="right">Year</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.movieList.map(movie => {
-            return (
-              <TableRow key={movie.id}>
-                <TableCell component="th" scope="row">
-                  {movie.title}
-                </TableCell>
-                <TableCell align="right">{movie.genre}</TableCell>
-                <TableCell align="right">{movie.age_rating}</TableCell>
-                <TableCell align="right">{movie.year}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
+        <MovieGridHead />
+        <MovieGridBody movieList={props.movieList} />
       </Table>
     </Paper>
   );
@@ -59,12 +35,14 @@ export class MoviesGridComponent extends React.Component<{}, State> {
   }
 
   componentDidMount() {
-    const movieList:MovieEntity[] = [
-        {id:0,title:'Star wars',genre:'chusta', age_rating:16, year:1989},
-        {id:1,title:'Star wars',genre:'chusta', age_rating:16, year:1989},
-        {id:2,title:'Star wars',genre:'chusta', age_rating:16, year:1989},
+    setTimeout(() => {
+      const movieList: MovieEntity[] = [
+        { id: 0, title: 'Star wars', genre: 'chusta', age_rating: 16, year: 1989 },
+        { id: 1, title: 'Black Panther', genre: 'chusta', age_rating: 18, year: 2017 },
+        { id: 2, title: 'Star wars', genre: 'chusta', age_rating: 16, year: 1989 },
       ];
-    this.setState({movieList})
+      this.setState({ movieList });
+    }, 500);
   }
 
   render() {
@@ -73,6 +51,3 @@ export class MoviesGridComponent extends React.Component<{}, State> {
     )
   }
 }
-/*
-export const MoviesGridComponent = () =>
-  <h3>Hello from Movies MoviesGridComponent</h3>*/
