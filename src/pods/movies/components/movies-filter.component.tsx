e5acec59ece563b6088373ce52dd4f9333577626
castelgo <oscar.castel_gormedino@roche.com>
2018-12-12 17:52:23 +0100

    Added filter fields

--- src/pods/movies/components/movies-filter.component.tsx
+++ src/pods/movies/components/movies-filter.component.tsx
@@ -1,4 +1,74 @@
 import * as React from 'react';
+import { TextField, Select, MenuItem, InputLabel, FormControl, createStyles, WithStyles } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
-export const MoviesFilterComponent = () => 
-  <h3>Hello from Movies MoviesFilterComponent</h3>
+const styles = theme => createStyles({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+interface Props extends WithStyles<typeof styles> {
+
+}
+
+const MoviesFilterComponentInner = (props: Props) => {
+
+    const {classes} = props;
+    
+    return (
+      <>
+        <div className={classes.container}>
+
+          <TextField
+            id="titleId"
+            label="Title"
+            margin="dense"
+            className={classes.textField}
+          />
+
+
+          <TextField
+            id="standard-select-currency"
+            select
+            label="Genere"
+            className={classes.textField}
+            //onChange={this.handleChange('currency')}
+            SelectProps={{
+              MenuProps: {
+                className: classes.menu,
+              },
+            }}
+            margin="normal"
+          >
+            <MenuItem value={10}>Option 1</MenuItem>
+            <MenuItem value={20}>Option 2</MenuItem>
+            <MenuItem value={30}>Option 3</MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            id="yearId"
+            label="Year"
+            className={classes.textField}
+          />
+
+        </div>
+      </>
+    )
+}
+
+export const MoviesFilterComponent = withStyles(styles)(MoviesFilterComponentInner);
---
+++ src/pods/movies/viewModel.ts
@@ -0,0 +1,5 @@
+export interface MovieFilter {
+    title: string;
+    genere: string;
+    year: number;
+}
