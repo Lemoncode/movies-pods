9418a40fb0ddc07ec86093e561d4286592192b7b
Martin-Pero, Ana {DSDI~Sant Cugat Dia} <ana.martin-pero@roche.com>
2018-12-13 09:15:12 +0100

    style

--- src/pods/movies/reducer/movies.reducer.ts
+++ src/pods/movies/reducer/movies.reducer.ts
@@ -1,7 +1,7 @@
 import { MovieEntity } from "../../../core/store/model";
 import { actionsEnums } from "../../../common-app/actions";
 
-export type movieState =  MovieEntity[];
+export type moviesState =  MovieEntity[];
 
 export function createEmptyMovieArray () {
   const moviesArrary = new Array();
@@ -9,16 +9,16 @@ export function createEmptyMovieArray () {
   return moviesArrary;
 }
 
-export const moviesReducer =  (state : MovieEntity[] = createEmptyMovieArray(), action) => {
+export const moviesReducer =  (state : moviesState = createEmptyMovieArray(), action) => {
     switch (action.type) {
       case actionsEnums.MOVIES_REQUEST_COMPLETED:
-        return handleMoviesRequestCompletedAction(state, action.payload);
+        return handleMoviesRequestCompletedAction(action.payload);
     }
   
     return state;
 };
 
-const handleMoviesRequestCompletedAction = (state : MovieEntity[], movies):MovieEntity[] => {
+const handleMoviesRequestCompletedAction = (movies:moviesState):moviesState => {
     return movies;
 }
   
