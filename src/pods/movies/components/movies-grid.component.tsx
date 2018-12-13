59aa1f7b3dd14fb9a5493a8390836aba26823563
jimenc23 <cristian.jimenez@roche.com>
2018-12-13 12:24:39 +0100

    solved conflicts

--- src/pods/movies/components/movies-grid.component.tsx
+++ src/pods/movies/components/movies-grid.component.tsx
@@ -3,9 +3,9 @@ import Table from '@material-ui/core/Table';
 import Paper from '@material-ui/core/Paper';
 import { MovieGridHeadContent } from './grid/movies-grid-head.component';
 import { MovieGridBodyContent } from './grid/movies-grid-body.component';
-import { MovieEntity } from '../viewModel';
 import { moviesAPI } from '../../../api/movies-api';
 import { mapFromMovieApiToMovieViewModel, mapFromMovieCollectionVMToMovieViewModel } from '../mapper';
+import { MovieEntity } from './view-model';
 
 interface Props {
   movieList: MovieEntity[];
