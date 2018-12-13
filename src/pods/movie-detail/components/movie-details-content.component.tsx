deef79f503fc779f121b84ef5a70191f9e7d932e
Oscar Garcia Masia <oscar.garcia.og3@roche.com>
2018-12-13 10:20:26 +0100

    responsiveness

--- src/pods/movie-detail/components/movie-details-content.component.tsx
+++ src/pods/movie-detail/components/movie-details-content.component.tsx
@@ -7,18 +7,28 @@ const styles = theme => createStyles({
     root: {
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        flexWrap: 'wrap',
+        padding: '0rem 0rem 3rem 0',
+        boxSizing:"border-box"
     },
     detailTitle: {
         fontWeight: 'bold',
     },
     media: {
-        height: '15rem',
-        width: '10rem'
+        height: 'auto',
+        flexBasis: '20%',
+        backgroundRepeat: 'no-repeat',
+        flexGrow: 1,
+        minHeight:'332px',
+        minWidth:'220px',
+        boxSizing:"border-box"
     },
     movieBody: {
         textAlign: 'justify',
-        width: '90%'
+        flexBasis: '80%',
+        padding: '2rem 3rem',
+        flexGrow: 1,
+        boxSizing:"border-box",
     }
 });
 
@@ -29,16 +39,14 @@ interface Props extends WithStyles<typeof styles> {
 const MovieDetailsContentInner = (props: Props) => {
 
     return (
+        
         <CardContent>
             <div className={props.classes.root}>
-                <CardMedia
-                    className={props.classes.media}
-                    image={props.movie.posterUrl}
-                    title={props.movie.title}
-                />
+                <div className={props.classes.media} style={{backgroundImage: `url(${props.movie.posterUrl})`}}/>
                 <div className={props.classes.movieBody}>
-                    <Typography variant={'h6'}>Genre: </Typography>{props.movie.genre}
-
+                    <span style={{display:'flex', alignItems:'baseline'}}><Typography variant={'h6'}>Genre:</Typography>&nbsp;&nbsp;&nbsp;<Typography variant={'subheading'}>{props.movie.genre}</Typography></span>
+                    <span style={{display:'flex', alignItems:'baseline'}}><Typography variant={'h6'}>Year:</Typography>&nbsp;&nbsp;&nbsp;<Typography variant={'subheading'}>{props.movie.year}</Typography></span>
+                    <span style={{display:'flex', alignItems:'baseline'}}><Typography variant={'h6'}>Age rating:</Typography>&nbsp;&nbsp;&nbsp;<Typography variant={'subheading'}>{props.movie.ageRating}&nbsp; years</Typography></span>
                     <Typography variant={'h6'}>Synopsis </Typography>
                     <Typography paragraph={true}>
                         <span>{props.movie.synopsis}</span>
@@ -72,5 +80,10 @@ export const MovieDetailsContent = withStyles(styles)(MovieDetailsContentInner);
         </div>
     </div>
 </CardContent>
-
+<CardMedia
+                    className={props.classes.media}
+                    image={props.movie.posterUrl}
+                    title={props.movie.title}
+                />
+                
     */
