3a0b5fbdff9015d6db18167421fdb1bd34ced9b1
Oscar Garcia Masia <oscar.garcia.og3@roche.com>
2018-12-13 15:52:31 +0100

    added test case

--- src/pods/movie-detail/movie-details.container.business.spec.ts
+++ src/pods/movie-detail/movie-details.container.business.spec.ts
@@ -18,4 +18,20 @@ describe('pods/movie-details.business', () => {
         //Assert
         expect(movieId).toEqual(0);
     });
+    it('should return movieId given a larger parts url with no Id', () => {
+        //Arrange
+        const url = '/movies/movie-detail/';
+        //Act
+        const movieId = getMovieId(url)
+        //Assert
+        expect(movieId).toEqual(0);
+    });
+    it('should return movieId given a larger parts url with Id', () => {
+        //Arrange
+        const url = '/movies/movie-detail/3';
+        //Act
+        const movieId = getMovieId(url)
+        //Assert
+        expect(movieId).toEqual(3);
+    });
 });
