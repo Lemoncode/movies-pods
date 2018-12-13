ea397bb1f26724820443e4e877c0df11b21a0b8d
Martin-Pero, Ana {DSDI~Sant Cugat Dia} <ana.martin-pero@roche.com>
2018-12-13 10:04:31 +0100

    review changes

--- src/pods/movies/reducer/movies.reducer.spec.ts
+++ src/pods/movies/reducer/movies.reducer.spec.ts
@@ -1,9 +1,9 @@
 import { moviesReducer } from './movies.reducer';
-import { MovieEntity } from '../../../api/model';
 import { actionsEnums } from '../../../common-app/actions';
+const deepFreeze = require('deep-freeze'); 
 
 describe('movies/reducers/moviesReducer tests', () => {
-    it(`should return default movie array when passing undefined state and some action type`, () => {
+    it(`should return empty movie collection when passing undefined state and some action type`, () => {
         //Arrange
         const state = undefined;
         const action = { type: 'some type' };
@@ -12,53 +12,25 @@ describe('movies/reducers/moviesReducer tests', () => {
         const nextState = moviesReducer(state, action);
 
         //Assert
-        expect(nextState[0].ageRating).toEqual(0);
-        expect(nextState[0].genre).toEqual('');
-        expect(nextState[0].poster).toEqual('');
-        expect(nextState[0].synopsis).toEqual('');
-        expect(nextState[0].title).toEqual('');
-        expect(nextState[0].year).toEqual(0);
-
+        expect(nextState.length).toEqual(0);
+   
     });
 
-    it(`should return updated state without mutate it when passing state, UPDATE_LOGIN_ENTITY_FIELD
-    action type and password field payload`, () => {
+    it(`should return updated state without mutate it when passing state, MOVIES_REQUEST_COMPLETED
+    action type and payload with several movies`, () => {
         //Arrange
-        // Initial array
-        const movie:MovieEntity = {
-            ageRating:12,
-            genre:'terror',
-            poster:'poster',
-            synopsis:'synopsis',
-            title:'movie title',
-            year:123
-        }
-        const moviesArrary = new Array();
-        moviesArrary.push(moviesArrary);
-
-        //new array
-        const movie2:MovieEntity = {
-            ageRating:13,
-            genre:'sci',
-            poster:'poster 2',
-            synopsis:'synopsis 2',
-            title:'movie title 2',
-            year:124
-        }
-        moviesArrary.push(moviesArrary);
-
-
+        const state = new Array({ageRating:12,genre:'terror',poster:'poster',synopsis:'synopsis',title:'movie title',year:123},
+                                    {ageRating:13,genre:'sci',poster:'poster',synopsis:'synopsis2',title:'movie title2',year:124});
         const action = {
-            type: actionsEnums.FILTER_REQUEST_COMPLETED,
-            payload: {moviesArrary}
+            type: actionsEnums.MOVIES_REQUEST_COMPLETED,
+            payload: {state}
             };
-            
+        deepFreeze(state);
+
         //Act
-        const nextState = moviesReducer(moviesArrary, action);
+        const nextState = moviesReducer(state, action);
 
         // Assert
-        expect(nextState.length).toEqual(2);
-    })
-            
-
+        expect(nextState).toEqual(state);
+    });
 })
